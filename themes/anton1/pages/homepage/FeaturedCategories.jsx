b8cc5ce883ec9442351f615a9277f5cf4dc8d827
aleksandarvolos <avolos@apple.com>
2024-06-11 18:15:07 +0200

    version 1.1.0

---
+++ themes/anton1/pages/homepage/FeaturedCategories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function FeaturedCategories() {
+  return (
+    <div className="page-width">
+    </div>
+  );
+}
+
+export default FeaturedCategories;
+
+export const layout = {
+  areaId: "content",
+  sortOrder: 5,
+};
